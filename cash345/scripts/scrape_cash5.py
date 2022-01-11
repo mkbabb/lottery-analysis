7de64cd73cb86e3cdd21c728fa019259b9a5dd16
Mike Babb <mike7400@gmail.com>
2022-01-11 10:18:03 -0500

    updated cash 5 scraper

--- cash345/scripts/scrape_cash5.py
+++ cash345/scripts/scrape_cash5.py
@@ -4,48 +4,60 @@ import urllib.request
 
 import pandas as pd
 from lxml import etree
+import re
 
-from utils import dollar_to_float
 
 from typing import *
-from utils import file_components
 
-CALLS_PER_SECOND = 0.1
+
+CALLS_PER_SECOND = 0.5
 
 CASH5_START = datetime.datetime.strptime("10/27/2006", "%m/%d/%Y")
 
 
-PATHS = {"prize_5": '//*[@id="ctl00_MainContent_lblCash5Match5Prize"]',
-         "prize_4": '//*[@id="ctl00_MainContent_lblCash5Match4Prize"]',
+PATHS = {
+    "prize_5": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl00_lblPrize5"]',
+    "prize_4": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl00_lblPrize4"]',
+    "winners_5": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl00_lblWin5"]',
+    "winners_4": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl00_lblWin4"]',
+    "winners_3": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl00_lblWin3"]',
+    "winners_2": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl00_lblWin2"]',
+    "prize_5_double_play": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl01_lblPrize5"]',
+    "prize_4_double_play": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl01_lblPrize5"]',
+    "winners_5_double_play": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl01_lblWin5"]',
+    "winners_4_double_play": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl01_lblWin4"]',
+    "winners_3_double_play": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl01_lblWin3"]',
+    "winners_2_double_play": '//*[@id="ctl00_MainContent_ResultsCash5_rptCash5_ctl01_lblWin2"]',
+}
 
-         "winners_5": '//*[@id="ctl00_MainContent_lblCash5Match5"]',
-         "winners_4": '//*[@id="ctl00_MainContent_lblCash5Match4"]',
-         "winners_3": '//*[@id="ctl00_MainContent_lblCash5Match3"]',
-         "winners_2": '//*[@id="ctl00_MainContent_lblCash5Match2"]',
+HEADER = {"User-Agent": "Lottery Research 1.0.0"}
 
-         "jackpot": '//*[@id="ctl00_MainContent_lblCash5TopPrize"]'}
 
-HEADER = {'User-Agent': 'Lottery Research 0.9.0'}
+def dollar_to_int(s: str) -> Optional[int]:
+    s = s.replace(",", "")
+    if (dollars := re.match("\$(\d+)", s)) is not None:
+        return int(dollars.group(1))
+    else:
+        return None
 
 
 def query_nclotto(date_string):
-    url = f"https://nclottery.com/Cash5?dd={date_string}"
+    url = f"https://nclottery.com/Cash5Pay?dd={date_string}"
     req = urllib.request.Request(url, headers=HEADER)
     response = urllib.request.urlopen(req)
-    data = response.read().decode('utf-8')
+    data = response.read().decode("utf-8")
 
     return etree.HTML(data)
 
 
-def scrape_cash5(out_path: str,
-                 start_date: datetime.time,
-                 end_date: datetime.time):
-    total_days = (end_date - start_date).days
-    data_dict = {key: [] for key in PATHS.keys()}
-    data_dict["date"] = []
+def scrape_cash5(out_path: str, start_date: datetime.time, end_date: datetime.time):
+    data_dict = {"date": []}
+    data_dict.update({key: [] for key in PATHS.keys()})
 
-    for n, date in enumerate((start_date + datetime.timedelta(i)
-                              for i in range(total_days))):
+    total_days = (end_date - start_date).days
+    dates = (start_date + datetime.timedelta(i) for i in range(total_days))
+
+    for date in dates:
         date_string = date.strftime("%m/%d/%Y")
         cash5_html = query_nclotto(date_string)
         data_dict["date"].append(date_string)
@@ -55,26 +67,27 @@ def scrape_cash5(out_path: str,
                 node = cash5_html.xpath(path)[0]
                 value = node.text
 
-                dollars = dollar_to_float(value)
-                if (dollars is not None):
+                dollars = dollar_to_int(value)
+
+                if dollars is not None:
                     data_dict[key].append(str(dollars))
                 else:
                     data_dict[key].append(value)
+
             except Exception as e:
-                print(e)
+                data_dict[key].append("0.0")
 
         time.sleep(CALLS_PER_SECOND)
 
     df = pd.DataFrame(data_dict)
-    df = df.reindex(list(sorted(df.columns)),
-                    axis=1)
     df.to_csv(out_path, index=False)
     return df
 
 
-start_date = datetime.datetime.strptime("11/27/2019", "%m/%d/%Y")
+start_date = datetime.datetime.strptime("10/27/2006", "%m/%d/%Y")
+# start_date = datetime.datetime.strptime("01/01/2022", "%m/%d/%Y")
 end_date = datetime.datetime.now()
 
-out_path = f"cash345/data/cash5_scraped.csv"
+out_path = f"cash345/data/cash5_scraped_2022.csv"
 
 df = scrape_cash5(out_path, start_date, end_date)
